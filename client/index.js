5775dd562c11fc7d2fcae12c500632fb495f9436
HipolitoE <espositohipolito@gmail.com>
2023-06-26 12:14:05 -0300

    mod cliente

--- client/index.js
+++ client/index.js
@@ -3,30 +3,9 @@ const axios = require("axios");
 const url = require("url");
 
 async function makeRequest() {
-    let payload = { name: "Jhon", email: "jhonny@santex.com" };
-
-    /*const params = new url.URLSearchParams(payload);
-
-    let config = {
-        method: "get",
-        url: `http://localhost:8080/user?${params}`,
-    };
-
-
-    let config = {
-        method: "post",
-        url: `http://localhost:8080/user`,
-        data: payload,
-    };
-
-    let response = await axios(config);*/
+    let payload = { name: "admin", email: "admin" };
     const response = await axios.post("http://localhost:8080/user", payload);
-    
     console.log("Datos del server:", response.data);
 }
 
 makeRequest();
-
-/*axios.get('http://localhost:8080/user/1234').then(res => {
-    console.log('Datos del Server:', res.data)
-});*/
